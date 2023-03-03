5cbe50cb90f4fd7a8e13e51f5e48d9795cab89d5
Faith Ye <faith.ye94@gmail.com>
2023-03-04 02:22:52 +0800

    added modals for add and filter icon when in mobile

--- apps/client/src/components/layout/LoggedInNavBar.tsx
+++ apps/client/src/components/layout/LoggedInNavBar.tsx
@@ -1,14 +1,25 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import {} from '../../app/slices/itemsSlice';
 import { useAppDispatch, useAppSelector } from '../../app/store';
+import { Dialog, Transition } from '@headlessui/react';
 
 const LoggedInNavBar = () => {
   const dispatch = useAppDispatch();
   const [navStatus, setNavStatus] = useState('');
   const [hamburgMenuStatus, setHamburgMenuStatus] = useState('hidden');
+  let [addIsOpen, setAddIsOpen] = useState(false);
+
   const navigate = useNavigate();
   let divRef = useRef<any>();
+
+  function closeAddModal() {
+    setAddIsOpen(false);
+  }
+
+  function openAddModal() {
+    setAddIsOpen(true);
+  }
 
   useEffect(() => {
     const handler = (event: any) => {
@@ -55,58 +66,18 @@ const LoggedInNavBar = () => {
           </div>
         </div>
         {/* Menu Items */}
-        <div className='flex space-x-5 md:text-l lg:text-xl tracking-wide items-center align-center justify-between'>
-          {/* <div className=" text-fontGreen">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-5 h-5 md:w-8 md:h-8 md:inline"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
-                />
-              </svg>
-              <div className="md:inline text-sm">{rottten.length}</div>
-            </div>
-            <div className="text-sadFace">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-5 h-5 md:w-8 md:h-8 md:inline"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.182 16.318A4.486 4.486 0 0012.016 15a4.486 4.486 0 00-3.198 1.318M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
-                />
-              </svg>
-              <div className="md:inline text-sm">{evergreen.length}</div>
-            </div>
-            <div className="text-fontGray">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-5 h-5 md:w-7 md:h-7 md:inline"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                />
-              </svg>
-              <div className="md:inline text-sm">{trashed.length}</div>
-            </div> */}
+        <div className='flex space-x-3 md:text-l lg:text-xl tracking-wide items-center align-center justify-between'>
+          <img
+            className='flex lg:hidden hover:cursor-pointer'
+            src='images/navbar/add_icon.svg'
+            onClick={openAddModal}
+          />
+          <img
+            className='flex lg:hidden hover:cursor-pointer'
+            src='images/navbar/filter_icon.svg'
+            onClick={() => console.log('filter icon clicked')}
+          />
+
           <div ref={divRef}>
             {/* Hambuger Icon */}
             <button
@@ -169,6 +140,60 @@ const LoggedInNavBar = () => {
           </div>
         </div>
       </div>
+      <Transition appear show={addIsOpen} as={Fragment}>
+        <Dialog as='div' className='relative z-10' onClose={closeAddModal}>
+          <Transition.Child
+            as={Fragment}
+            enter='ease-out duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <div className='fixed inset-0 bg-black bg-opacity-25' />
+          </Transition.Child>
+
+          <div className='fixed inset-0 overflow-y-auto'>
+            <div className='flex min-h-full items-center justify-center p-4 text-center'>
+              <Transition.Child
+                as={Fragment}
+                enter='ease-out duration-300'
+                enterFrom='opacity-0 scale-95'
+                enterTo='opacity-100 scale-100'
+                leave='ease-in duration-200'
+                leaveFrom='opacity-100 scale-100'
+                leaveTo='opacity-0 scale-95'
+              >
+                <Dialog.Panel className='w-full max-w-md flex flex-col items-center justify-center transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
+                  <Dialog.Title
+                    as='h3'
+                    className='flex text-lg font-medium leading-6 text-red-400'
+                  >
+                    What would you like to add?
+                  </Dialog.Title>
+                  <div className='mt-4'>
+                    <button
+                      type='button'
+                      className='mr-2 inline-flex justify-center rounded-2xl border border-transparent bg-orange px-4 py-2 text-sm font-medium text-white hover:bg-gradient-to-r from-orange to-pink focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2'
+                      onClick={closeAddModal}
+                    >
+                      Add Item
+                    </button>
+                    <button
+                      type='button'
+                      className='mr-2 inline-flex justify-center rounded-2xl border border-transparent bg-orange px-4 py-2 text-sm font-medium text-white hover:bg-gradient-to-r from-orange to-pink focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2'
+                      onClick={closeAddModal}
+                    >
+                      Add Category
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </nav>
   );
 };
