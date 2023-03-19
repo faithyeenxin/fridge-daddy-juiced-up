929b303adc20fce48ad24e5bd0ecfa2c9847888e
Faith Ye <faith.ye94@gmail.com>
2023-03-19 22:18:58 +0800

    completed with all the functionality

--- apps/client/src/pages/LoginRedirect.tsx
+++ apps/client/src/pages/LoginRedirect.tsx
@@ -2,9 +2,9 @@ import React, { FC } from 'react';
 
 const LoginRedirect: FC = () => {
   return (
-    <div className='w-[100vw] h-[80vh] flex flex-col gap-5 justify-center items-center text-orange text-xl font-lora'>
-      <div>You have been idle for too long.</div>
-      <div>Please sign in again!</div>
+    <div className='h-[80vh] flex flex-col gap-5 justify-center items-center text-orange ∂text-md md:text-xl font-lora'>
+      <div className='text-center'>You have been idle for too long.</div>
+      <div className='text-center'>Please sign in again!</div>
     </div>
   );
 };
--- apps/client/src/pages/RegisterRedirect.tsx
+++ apps/client/src/pages/RegisterRedirect.tsx
@@ -2,9 +2,14 @@ import React, { FC } from 'react';
 
 const RegisterRedirect: FC = () => {
   return (
-    <div className='w-[100vw] h-[80vh] flex justify-center items-center text-orange text-xl font-lora'>
-      You do not have an account with FridgeDaddy, join the family by clicking
-      Get Started button on the top right of this website!
+    <div className='h-[80vh] flex flex-col gap-5 justify-center items-center text-orange text-md md:text-xl font-lora'>
+      <div className='text-center'>
+        You do not have an account with FridgeDaddy
+      </div>
+      <div className='text-center'>
+        Join the family by clicking Get Started button on the top right of this
+        website!
+      </div>
     </div>
   );
 };
