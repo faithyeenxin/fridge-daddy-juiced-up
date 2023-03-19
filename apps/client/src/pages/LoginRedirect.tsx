bbd7571709e1b1c19d3acd6df3ee0d32a439ba01
Faith Ye <faith.ye94@gmail.com>
2023-03-19 22:11:31 +0800

    completed redirect pages

--- apps/client/src/pages/LoginRedirect.tsx
+++ apps/client/src/pages/LoginRedirect.tsx
@@ -1,7 +1,12 @@
-import React, { FC } from "react";
+import React, { FC } from 'react';
 
 const LoginRedirect: FC = () => {
-  return <div>Login Redirect</div>;
+  return (
+    <div className='w-[100vw] h-[80vh] flex flex-col gap-5 justify-center items-center text-orange text-xl font-lora'>
+      <div>You have been idle for too long.</div>
+      <div>Please sign in again!</div>
+    </div>
+  );
 };
 
 export default LoginRedirect;
--- apps/client/src/pages/RegisterRedirect.tsx
+++ apps/client/src/pages/RegisterRedirect.tsx
@@ -1,7 +1,12 @@
-import React, { FC } from "react";
+import React, { FC } from 'react';
 
 const RegisterRedirect: FC = () => {
-  return <div>Register Redirect</div>;
+  return (
+    <div className='w-[100vw] h-[80vh] flex justify-center items-center text-orange text-xl font-lora'>
+      You do not have an account with FridgeDaddy, join the family by clicking
+      Get Started button on the top right of this website!
+    </div>
+  );
 };
 
 export default RegisterRedirect;
