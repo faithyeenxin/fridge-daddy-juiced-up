6bb34efdba0d765aaea28273b1dae54d41cbcc4c
Faith Ye En Xin <faith.ye94@gmail.com>
2023-09-05 15:28:12 +0800

    fix(database): updated database url to not use aws

--- apps/server/controller/UserController.js
+++ apps/server/controller/UserController.js
@@ -1,65 +1,67 @@
-const express = require('express');
+/** @format */
+
+const express = require("express");
 const router = express.Router();
 
-const SECRET = process.env.SECRET ?? 'faithmadethis';
+const SECRET = process.env.SECRET ?? "faithmadethis";
 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 //* Seed Route
 
-router.get('/seed', async (req, res) => {
-  await prisma.user.deleteMany({});
+router.get("/seed", async (req, res) => {
+  // await prisma.user.deleteMany({});
   const seedUsers = await prisma.user.createMany({
     data: [
       {
-        name: 'Test User',
-        email: 'testUser@hotmail.com',
-        password: bcrypt.hashSync('Password123!', 10),
-        image: 'https://i.mydramalist.com/eJNje_5c.jpg',
+        name: "Test User",
+        email: "testUser@hotmail.com",
+        password: bcrypt.hashSync("Password123!", 10),
+        image: "https://i.mydramalist.com/eJNje_5c.jpg",
         dateJoined: new Date(2022, 10, 0o5),
       },
       {
-        name: 'Administrator',
-        email: 'admin123@hotmail.com',
-        password: bcrypt.hashSync('password123', 10),
+        name: "Administrator",
+        email: "admin123@hotmail.com",
+        password: bcrypt.hashSync("password123", 10),
         image:
-          'https://res.cloudinary.com/dj6tlm5xx/image/upload/v1665411728/samples/people/new_user_fnx00w.jpg',
+          "https://res.cloudinary.com/dj6tlm5xx/image/upload/v1665411728/samples/people/new_user_fnx00w.jpg",
         dateJoined: new Date(2022, 10, 0o5),
       },
       {
-        name: 'Benjamine',
-        email: 'benjamine123@hotmail.com',
-        password: bcrypt.hashSync('ben123', 10),
+        name: "Benjamine",
+        email: "benjamine123@hotmail.com",
+        password: bcrypt.hashSync("ben123", 10),
         image:
-          'https://res.cloudinary.com/dj6tlm5xx/image/upload/v1665411728/samples/people/new_user_fnx00w.jpg',
+          "https://res.cloudinary.com/dj6tlm5xx/image/upload/v1665411728/samples/people/new_user_fnx00w.jpg",
         dateJoined: new Date(2022, 10, 0o2),
       },
       {
-        name: 'Chelsea',
-        email: 'chealsea123@hotmail.com',
-        password: bcrypt.hashSync('chelsea123', 10),
+        name: "Chelsea",
+        email: "chealsea123@hotmail.com",
+        password: bcrypt.hashSync("chelsea123", 10),
         image:
-          'https://res.cloudinary.com/dj6tlm5xx/image/upload/v1665411728/samples/people/new_user_fnx00w.jpg',
+          "https://res.cloudinary.com/dj6tlm5xx/image/upload/v1665411728/samples/people/new_user_fnx00w.jpg",
         dateJoined: new Date(2022, 10, 0o3),
       },
       {
-        name: 'Dominique',
-        email: 'dominique123@hotmail.com',
-        password: bcrypt.hashSync('dom123', 10),
+        name: "Dominique",
+        email: "dominique123@hotmail.com",
+        password: bcrypt.hashSync("dom123", 10),
         image:
-          'https://res.cloudinary.com/dj6tlm5xx/image/upload/v1665411728/samples/people/new_user_fnx00w.jpg',
+          "https://res.cloudinary.com/dj6tlm5xx/image/upload/v1665411728/samples/people/new_user_fnx00w.jpg",
         dateJoined: new Date(2022, 10, 0o4),
       },
       {
-        name: 'Faith',
-        email: 'faith123@hotmail.com',
-        password: bcrypt.hashSync('Faithyeenx5!', 10),
+        name: "Faith",
+        email: "faith123@hotmail.com",
+        password: bcrypt.hashSync("Faithyeenx5!", 10),
         image:
-          'https://res.cloudinary.com/dj6tlm5xx/image/upload/v1665411728/samples/people/new_user_fnx00w.jpg',
+          "https://res.cloudinary.com/dj6tlm5xx/image/upload/v1665411728/samples/people/new_user_fnx00w.jpg",
         dateJoined: new Date(2022, 10, 0o1),
       },
     ],
@@ -69,13 +71,13 @@ router.get('/seed', async (req, res) => {
 });
 
 //* Show All Users
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   const allUsers = await prisma.user.findMany();
   res.status(200).send(allUsers);
 });
 
 //* Show By ID
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
   const user = await prisma.user.findUnique({
     where: {
@@ -85,7 +87,7 @@ router.get('/:id', async (req, res) => {
   if (user) {
     res.status(200).send(user);
   } else {
-    res.status(400).send({ error: 'User not found' });
+    res.status(400).send({ error: "User not found" });
   }
 });
 
@@ -101,7 +103,7 @@ router.get('/:id', async (req, res) => {
 // });
 
 //* Find by Email
-router.get('/findByEmail/:email', async (req, res) => {
+router.get("/findByEmail/:email", async (req, res) => {
   const { email } = req.params;
   const user = await prisma.user.findMany({ where: { email: email } });
   if (user.length === 0) {
@@ -112,29 +114,27 @@ router.get('/findByEmail/:email', async (req, res) => {
 });
 
 //* Create User
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const userData = req.body;
   const userExists = await prisma.user.findUnique({
     where: { email: userData.email },
   });
   if (userExists !== null) {
-    res
-      .status(400)
-      .send({
-        error: 'User already exist, please register with another email.',
-      });
+    res.status(400).send({
+      error: "User already exist, please register with another email.",
+    });
   } else {
     newUser.password = bcrypt.hashSync(newUser.password, 10);
     const userCreated = await prisma.user.create({
       data: newUser,
     });
-    const token = jwt.sign(userCreated, SECRET, { expiresIn: '30m' });
+    const token = jwt.sign(userCreated, SECRET, { expiresIn: "30m" });
     res.status(200).send({ token: token });
   }
 });
 
 //* Create/Login Google User
-router.post('/google', async (req, res) => {
+router.post("/google", async (req, res) => {
   try {
     const googleUser = req.body;
     const existingUser = await prisma.user.findUnique({
@@ -151,19 +151,19 @@ router.post('/google', async (req, res) => {
           email: `${googleUser.email}`,
         },
       });
-      const token = jwt.sign(user, SECRET, { expiresIn: '30m' });
+      const token = jwt.sign(user, SECRET, { expiresIn: "30m" });
       res.status(200).send({ token: token });
     } else {
-      const token = jwt.sign(existingUser, SECRET, { expiresIn: '30m' });
+      const token = jwt.sign(existingUser, SECRET, { expiresIn: "30m" });
       res.status(200).send({ token: token });
     }
   } catch {
-    res.status(400).send({ err: 'There is an error' });
+    res.status(400).send({ err: "There is an error" });
   }
 });
 
 //* User LOGIN
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const user = await prisma.user.findUnique({
     where: {
@@ -171,17 +171,17 @@ router.post('/login', async (req, res) => {
     },
   });
   if (user === null) {
-    res.status(400).send({ error: 'User Not Found' });
+    res.status(400).send({ error: "User Not Found" });
   } else if (bcrypt.compareSync(password, user.password)) {
-    const token = jwt.sign(user, SECRET, { expiresIn: '30m' });
+    const token = jwt.sign(user, SECRET, { expiresIn: "30m" });
     res.status(200).send({ token: token });
   } else {
-    res.status(400).send({ error: 'Wrong password' });
+    res.status(400).send({ error: "Wrong password" });
   }
 });
 
 //* User LOGIN GOOGLE
-router.post('/login-google', async (req, res) => {
+router.post("/login-google", async (req, res) => {
   const { email } = req.body;
   const user = await prisma.user.findUnique({
     where: {
@@ -189,15 +189,15 @@ router.post('/login-google', async (req, res) => {
     },
   });
   if (user === null) {
-    res.status(400).send({ error: 'User Not Found' });
+    res.status(400).send({ error: "User Not Found" });
   } else {
-    const token = jwt.sign(user, SECRET, { expiresIn: '30m' });
+    const token = jwt.sign(user, SECRET, { expiresIn: "30m" });
     res.status(200).send({ token: token });
   }
 });
 
 //* User Change Password
-router.put('/change-password', async (req, res) => {
+router.put("/change-password", async (req, res) => {
   const { id, password, newPassword } = req.body;
   const user = await prisma.user.findUnique({
     where: {
@@ -205,7 +205,7 @@ router.put('/change-password', async (req, res) => {
     },
   });
   if (user === null) {
-    res.status(400).send({ error: 'User Not Found' });
+    res.status(400).send({ error: "User Not Found" });
   } else if (bcrypt.compareSync(password, user.password)) {
     const updatedData = user;
     updatedData.password = bcrypt.hashSync(newPassword, 10);
@@ -217,7 +217,7 @@ router.put('/change-password', async (req, res) => {
     });
     res.status(200).send(updatedUser);
   } else {
-    res.status(400).send({ error: 'Incorrect current password entered' });
+    res.status(400).send({ error: "Incorrect current password entered" });
   }
 });
 
@@ -235,7 +235,7 @@ router.put('/change-password', async (req, res) => {
 // });
 
 //* Delete User
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const items = await prisma.item.deleteMany({
